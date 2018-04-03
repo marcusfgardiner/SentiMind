73899f0a01addf9b32d0ce0da69216b4b8a91583
Stephen Tilley <stephentilley@gmail.com>
2018-04-03 15:55:13 +0100

    feat: Add a flask server :sparkles:

---
+++ fullstack_template/server/server.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/hello")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
