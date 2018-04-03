0367782280e02cc89283f105d465abd332dbe802
carlostrapet <trapet_llamas@hotmail.com>
2018-04-03 16:16:41 +0100

    :white_check_mark: test: correctly importing modules/files to test files

--- tests/test_sentiment.py
+++ tests/test_sentiment.py
@@ -1,5 +1,9 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath('..'))
+
 from mypkg import sentiment
 
 
 def test_answer():
-    assert polarity_score('I am terrible') == -1
+    assert sentiment.polarity_score('I am terrible') == -1
