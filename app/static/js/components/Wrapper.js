c1e6c3c032271d8d750b8f61e9add76deb2d2ba3
marcusfgardiner <marcusfgardiner@gmail.com>
2018-04-04 12:30:45 +0100

    :sparkles: feat: SubHeader renders in Wrapper

--- app/static/js/components/Wrapper.js
+++ app/static/js/components/Wrapper.js
@@ -1,11 +1,13 @@
 import React, { Component} from 'react';
 import Header from './Header';
+import SubHeader from './SubHeader';
 
 class Wrapper extends Component {
   render() {
     return (
       <div>
         <Header />
+        <SubHeader />
       </div>
     );
   }
