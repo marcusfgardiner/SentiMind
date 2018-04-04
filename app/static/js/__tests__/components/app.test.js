5f1d9aba35cc17b808f89a4da3e8d160f5917462
marcusfgardiner <marcusfgardiner@gmail.com>
2018-04-04 12:16:37 +0100

    :white_check_mark: test: App renders Header component

--- app/static/js/__tests__/components/app.test.js
+++ app/static/js/__tests__/components/app.test.js
@@ -4,7 +4,12 @@ import app from '../../components/app';
 
 describe('app', () => {
   const app = shallow(<app />);
+
   it('renders correctly', () => {
     expect(app).toMatchSnapshot();
   });
+
+  it('renders a Header component', () => {
+    expect(app.find('Header').exists()).toBe(true)
+  })
 });
