154606c8507241d42041f3d7de3575403f54d336
marcusfgardiner <marcusfgardiner@gmail.com>
2018-04-04 15:10:24 +0100

    :hankey: chore: working out webpack refresh issue

--- app/server/server.py
+++ app/server/server.py
@@ -18,23 +18,23 @@ def index():
 
 
 @app.route('/hello')
-def polarity(text):
-    score = polarity_score(text)
-    return polarity_result(score)
-
-
-def polarity_score(text):
-    string = TextBlob(text)
-    return string.sentiment.polarity
-
-
-def polarity_result(score):
-    if score == 0:
-        return "neutral"
-    elif score <= 0:
-        return "negative"
-    elif score >= 0:
-        return "positive"
+# def polarity(text):
+#     score = polarity_score(text)
+#     return polarity_result(score)
+#
+#
+# def polarity_score(text):
+#     string = TextBlob(text)
+#     return string.sentiment.polarity
+#
+#
+# def polarity_result(score):
+#     if score == 0:
+#         return "neutral"
+#     elif score <= 0:
+#         return "negative"
+#     elif score >= 0:
+#         return "positive"
 
 # sentiment.polarity('Donald trump sucks')
 
