5fc92030ea9a3fb0997f89993c8806e4b18efa6d
Daniel Keen <keendaniel01@gmail.com>
2018-04-05 14:36:55 +0100

    :white_check_mark: test: Output view renders button

--- frontend/src/__tests__/components/OutputView.test.js
+++ frontend/src/__tests__/components/OutputView.test.js
@@ -3,19 +3,23 @@ import { shallow } from 'enzyme';
 import OutputView from '../../components/OutputView';
 
 describe('OutputView', () => {
-  const outputView = shallow(<OutputView/>)
+  const outputView = shallow(<OutputView />);
 
   it('renders correctly', () => {
     expect(outputView).toMatchSnapshot();
-  })
+  });
 
   it('renders subheader component', () => {
-    expect(outputView.find('SubHeader').exists()).toBe(true)
-  })
+    expect(outputView.find('SubHeader').exists()).toBe(true);
+  });
 
   it('renders sentiment component', () => {
-    expect(outputView.find('Sentiment').exists()).toBe(true)
-  })
+    expect(outputView.find('Sentiment').exists()).toBe(true);
+  });
+
+  it('renders Button component', () => {
+    expect(outputView.find('Button').exists()).toBe(true);
+  });
 
   describe('SubHeader props', () => {
     it('passes flexible SubHeaderText down to SubHeader', () => {
@@ -24,4 +28,4 @@ describe('OutputView', () => {
       );
     });
   });
-})
+});
