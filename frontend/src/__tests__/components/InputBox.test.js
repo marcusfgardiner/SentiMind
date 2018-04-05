1113523e0ba76a61964966eae34aa4a28b94961e
Daniel Keen <keendaniel01@gmail.com>
2018-04-05 10:46:58 +0100

    :white_check_mark: test: INputBox calls handleTextBoxINput() on change

--- frontend/src/__tests__/components/InputBox.test.js
+++ frontend/src/__tests__/components/InputBox.test.js
@@ -3,9 +3,21 @@ import { shallow } from 'enzyme';
 import InputBox from '../../components/InputBox';
 
 describe('InputBox', () => {
-  const inputBox = shallow(<InputBox />);
+  const mockHandleTextBoxInput = jest.fn();
+  const props = { handleTextBoxInput: mockHandleTextBoxInput };
+  const inputBox = shallow(<InputBox {...props} />);
 
   it('renders correctly', () => {
     expect(inputBox).toMatchSnapshot();
-  })
-})
+  });
+
+  describe('when a user types into the input', () => {
+    beforeEach(() => {
+      inputBox.find('input').simulate('change');
+    });
+
+    it('calls the handleTextBoxInput function passed in from parent', () => {
+      expect(mockHandleTextBoxInput).toHaveBeenCalled();
+    });
+  });
+});
