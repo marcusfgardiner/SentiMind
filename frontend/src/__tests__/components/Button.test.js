7aa7fc00309d619d6a47ac213288dc4a72ca5e7a
Daniel Keen <keendaniel01@gmail.com>
2018-04-05 11:13:04 +0100

    :white_check_mark: test: Button component fires handleSubmit function passed down to it

--- frontend/src/__tests__/components/Button.test.js
+++ frontend/src/__tests__/components/Button.test.js
@@ -3,9 +3,21 @@ import { shallow } from 'enzyme';
 import Button from '../../components/Button';
 
 describe('Button', () => {
-  const button = shallow(<Button />);
+  const mockHandleSubmitClick = jest.fn();
+  const props = { handleSubmit: mockHandleSubmitClick };
+  const button = shallow(<Button {...props} />);
 
   it('renders correctly', () => {
     expect(button).toMatchSnapshot();
-  })
-})
+  });
+
+  describe('when clicked', () => {
+    beforeEach(() => {
+      button.find('button').prop('onClick')();
+    });
+
+    it('fires handleSubmit()', () => {
+      expect(mockHandleSubmitClick).toHaveBeenCalled();
+    });
+  });
+});
