6e2f7d67cce3a65df52c713f17a27a5be0374978
Daniel Keen <keendaniel01@gmail.com>
2018-04-05 14:33:44 +0100

    :recycle: refactor: button text from props

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Button = props => {
-  return <button onClick={props.handleSubmit}>Go!</button>;
+  return <button onClick={props.handleSubmit}>{props.buttonText}</button>;
 };
 
 export default Button;
