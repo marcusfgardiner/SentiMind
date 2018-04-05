695162547ec8a3c9f9c3b168dd32d05dbcbf3d11
carlostrapet <trapet_llamas@hotmail.com>
2018-04-05 12:02:52 +0100

    :white_check_mark: test: added unit tests for Tweet class initialization

---
+++ backend/mypkg/tweet.py
@@ -0,0 +1,8 @@
+class Tweet:
+   'Wraps #user, #text and #sentiment'
+
+   def __init__(self, user, text, sentiment):
+      self.user = user
+      self.text = text
+      self.sentiment = sentiment
+      
--- backend/tests/context.py
+++ backend/tests/context.py
@@ -4,3 +4,5 @@ sys.path.insert(0, os.path.abspath('..'))
 
 from mypkg import sentiment
 from mypkg import twitter_req
+from mypkg import tweet
+from mypkg.tweet import Tweet
