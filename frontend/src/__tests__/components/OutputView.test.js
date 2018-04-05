8470b8212372f6ffc9d0c100d52b85a4874aedc3
marcusfgardiner <marcusfgardiner@gmail.com>
2018-04-05 16:23:22 +0100

    :white_check_mark: test: passing props handle submit function to button from output view

--- frontend/src/__tests__/components/OutputView.test.js
+++ frontend/src/__tests__/components/OutputView.test.js
@@ -3,7 +3,9 @@ import { shallow } from 'enzyme';
 import OutputView from '../../components/OutputView';
 
 describe('OutputView', () => {
-  const outputView = shallow(<OutputView />);
+  const mockHandleSubmit = jest.fn();
+  const props = {handleSubmit: mockHandleSubmit}
+  const outputView = shallow(<OutputView {...props}/>);
 
   it('renders correctly', () => {
     expect(outputView).toMatchSnapshot();
@@ -34,6 +36,11 @@ describe('OutputView', () => {
       it('passes flexible buttonText down to Button', () => {
         expect(outputView.find('Button').prop('buttonText')).toBe(
           'Choose another topic'
+        );
+      });
+      it('handle submit function down to Button', () => {
+        expect(outputView.find('Button').prop('handleSubmit')).toBe(
+          mockHandleSubmit
         );
       });
     });
