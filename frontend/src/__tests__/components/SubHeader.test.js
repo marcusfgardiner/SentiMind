bcaade6cfb1e6b4d4ad5ff3b126666a4b31e8f82
marcusfgardiner <marcusfgardiner@gmail.com>
2018-04-05 12:27:00 +0100

    :white_check_mark: test: test written for rendering flexible subheader

--- frontend/src/__tests__/components/SubHeader.test.js
+++ frontend/src/__tests__/components/SubHeader.test.js
@@ -3,9 +3,14 @@ import { shallow } from 'enzyme';
 import SubHeader from '../../components/SubHeader';
 
 describe('SubHeader', () => {
-  const subHeader = shallow(<SubHeader />)
+  const props = {text: 'Flexible Subheader'}
+  const subHeader = shallow(<SubHeader {...props}/>)
 
   it('renders correctly', () => {
     expect(subHeader).toMatchSnapshot()
+  })
+
+  it('renders flexible subheader', () => {
+    expect(subHeader.find('h3').text()).toEqual('Flexible Subheader')
   })
 })
---
+++ frontend/src/components/OutputView.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OutputView = () => {
+  return (
+    <div>
+      <h1> Output View is Output View </h1>
+    </div>
+  )
+};
+
+export default OutputView
