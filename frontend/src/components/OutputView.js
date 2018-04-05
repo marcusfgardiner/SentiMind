0362bd67ff7375c056d7d16e97b3f54ec54c4ea4
marcusfgardiner <marcusfgardiner@gmail.com>
2018-04-05 12:46:49 +0100

    :sparkles: feat: sentiment view renders in OutputView

--- frontend/src/components/OutputView.js
+++ frontend/src/components/OutputView.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import SubHeader from './SubHeader'
+import Sentiment from './Sentiment'
 
 const OutputView = () => {
   return (
     <div>
       <SubHeader subHeaderText='So, the world thinks...'/>
+      <Sentiment />
     </div>
   )
 };
