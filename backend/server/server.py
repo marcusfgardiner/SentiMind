b7c589fd14b07007f9a27975d0ef96cb8930e197
carlostrapet <trapet_llamas@hotmail.com>
2018-04-07 16:30:16 +0100

    :building_construction: :hankey: chore: passing session between routes

--- backend/server/server.py
+++ backend/server/server.py
@@ -4,6 +4,8 @@ from flask import Flask
 from flask import jsonify
 from flask import Response
 from flask import request
+from flask import session
+
 from flask_cors import CORS, cross_origin
 
 import sys
@@ -15,19 +17,21 @@ from mypkg.sentiment_analyser import SentimentAnalyser
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/', methods=['GET'])
-def index():
-    sentiment_analyser = SentimentAnalyser()
-    params = 'ronaldo'
-    result = sentiment_analyser.general_polarity_result(params)
-    json_string = {"sentiment": "%(result)s" % locals()}
-    return Response(json.dumps(json_string), mimetype='application/json')
-
 @app.route('/sendjson', methods=['GET', 'POST'])
 def postJsonHandler():
     content = request.get_json()
+    session['query'] = content
     print(content)
     return 'done'
+
+@app.route('/', methods=['GET'])
+def index():
+    sentiment_analyser = SentimentAnalyser()
+    params = session.get('query', None)
+    print(params)
+    result = sentiment_analyser.general_polarity_result(params)
+    json_string = {"sentiment": "%(result)s" % locals()}
+    return Response(json.dumps(json_string), mimetype='application/json')
 
 if __name__ == '__main__':
     app.run()
