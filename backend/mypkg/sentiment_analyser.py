f006da8b975e29bba3eb9be4e158ca821b394df8
carlostrapet <trapet_llamas@hotmail.com>
2018-04-07 12:24:52 +0100

    :sparkles: feat: updated backend to accomodate custom search query. update params variable in server.py to customize query. lang defaults to en

--- backend/mypkg/sentiment_analyser.py
+++ backend/mypkg/sentiment_analyser.py
@@ -6,21 +6,21 @@ import json
 class SentimentAnalyser:
     'Requests tweets, cleans them, assigns sentiment and averages them'
 
-    def populate(self):
+    def populate(self, query):
       cleaned_tweets = []
       tweet_parser = TweetParser()
-      tweets = get_tweets()
+      tweets = get_tweets(query)
       for tweet in tweets:
           cleaned_tweets.append(tweet_parser.parse_tweet(tweet))
       return cleaned_tweets
 
-    def average_sentiment(self):
+    def average_sentiment(self, query):
       avg_sentiment = 0
-      tweets = self.populate()
+      tweets = self.populate(query)
       for tweet in tweets:
           avg_sentiment += round(tweet.sentiment, 2)
       return round(avg_sentiment/len(tweets), 2)
 
-    def general_polarity_result(self):
-        result = polarity_result(self.average_sentiment())
+    def general_polarity_result(self, query):
+        result = polarity_result(self.average_sentiment(query))
         return result
--- backend/mypkg/twitter_req.py
+++ backend/mypkg/twitter_req.py
@@ -6,9 +6,8 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-def get_tweets():
+def get_tweets(query):
     tweets = []
-    query = 'north korea'
     max_tweets = 10
     for tweet in tweepy.Cursor(api.search,q=query, lang='en').items(max_tweets):
         tweets.append(tweet._json)
