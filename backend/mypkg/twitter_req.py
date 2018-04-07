9287d021468135bee4892be48cf3981225f2e727
carlostrapet <trapet_llamas@hotmail.com>
2018-04-07 12:05:17 +0100

    :sparkles: chore: updated get_tweets method to accomodate lang, max_tweets and query

--- backend/mypkg/twitter_req.py
+++ backend/mypkg/twitter_req.py
@@ -8,6 +8,8 @@ api = tweepy.API(auth)
 
 def get_tweets():
     tweets = []
-    for tweet in tweepy.Cursor(api.search,q='north korea').items(10):
+    query = 'north korea'
+    max_tweets = 10
+    for tweet in tweepy.Cursor(api.search,q=query, lang='en').items(max_tweets):
         tweets.append(tweet._json)
     return tweets
