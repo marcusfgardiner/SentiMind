ff7e91c03668a77d6d325dcd7bff21de38da343c
Daniel Keen <keendaniel01@gmail.com>
2018-04-08 14:14:56 +0100

    :sparkles: feat: bar charts!

--- frontend/src/components/OutputView.js
+++ frontend/src/components/OutputView.js
@@ -2,12 +2,22 @@ import React from 'react';
 import SubHeader from './SubHeader';
 import Sentiment from './Sentiment';
 import Button from './Button';
-
+import { BarChart } from 'react-easy-chart';
 const OutputView = props => {
   return (
     <div>
       <SubHeader subHeaderText="So, the world thinks..." />
       <Sentiment sentiment={props.sentiment} />
+      <BarChart
+        axes
+        height={350}
+        width={650}
+        data={[
+          { x: 'Positive', y: 60, color: 'green' },
+          { x: 'Negative', y: 30, color: 'red' },
+          { x: 'Neutral', y: 10, color: 'yellow' }
+        ]}
+      />
       <Button
         buttonText="Choose another topic"
         handleSubmit={props.handleSubmit}
