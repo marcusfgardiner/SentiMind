b1a9bbd6483ce19118e448959effe3ce63903cbf
carlostrapet <trapet_llamas@hotmail.com>
2018-04-09 14:27:17 +0100

    :sparkles: feat: added sentiment_counter

--- backend/mypkg/sentiment_analyser.py
+++ backend/mypkg/sentiment_analyser.py
@@ -23,10 +23,23 @@ class SentimentAnalyser:
     def general_polarity_result(self, query):
         tweets = self.populate(query)
         average = self.average_polarity(tweets)
+        sentiments = self.sentiment_counter(tweets)
         result = polarity_result(average)
         positivity_percentage = self.polarity_converter(average)
-        json_string = {"polarity": "%(result)s" % locals(), "positivity_percentage": "%(positivity_percentage)s" % locals()}
+        json_string = {"sentiments": sentiments, "polarity": "%(result)s" % locals(
+        ), "positivity_percentage": "%(positivity_percentage)s" % locals()}
         return json_string
+
+    def sentiment_counter(self, tweets):
+      result = { "positive": 0, "neutral": 0, "negative": 0}
+      for tweet in tweets:
+        if tweet.sentiment == "positive":
+          result["positive"] += 1
+        elif tweet.sentiment == "neutral":
+          result["neutral"] += 1
+        else:
+          result["negative"] += 1
+      return result    
 
     def polarity_converter(self, polarity):
       return (polarity + 1) * 50
--- backend/tests/test_sentiment_analyser.py
+++ backend/tests/test_sentiment_analyser.py
@@ -8,10 +8,10 @@ with open('./helpers/tweet_dummy.json') as json_file:
 
 
 def test_sentiment_analyser():
-  sentiment_analyser = SentimentAnalyser()
-  query = 'easter'
-  populate = sentiment_analyser.populate(query)
-  assert isinstance(populate[0], Tweet)
+    sentiment_analyser = SentimentAnalyser()
+    query = 'easter'
+    populate = sentiment_analyser.populate(query)
+    assert isinstance(populate[0], Tweet)
 
 def test_average_sentiment():
     tweet = Tweet("@dumb", "heyo")
@@ -21,10 +21,17 @@ def test_average_sentiment():
     avg_polarity = sentiment_analyser.average_polarity(tweets)
     assert isinstance(avg_polarity, (int, float, complex))
 
+def test_sentiment_counter():
+  tweet = Tweet("@dumb", "heyo")
+  tweet.sentiment = "positive"
+  tweets = [tweet]
+  sentiment_analyser = SentimentAnalyser()
+  result = sentiment_analyser.sentiment_counter(tweets)
+  assert result["positive"] == 1
 
 def test_polarity_converter():
-  sentiment_analyser = SentimentAnalyser()
-  assert sentiment_analyser.polarity_converter(-0.5) == 25
+    sentiment_analyser = SentimentAnalyser()
+    assert sentiment_analyser.polarity_converter(-0.5) == 25
 
 def test_general_polarity_result():
     query = 'easter'
