39e85774626b9a08d12b5ee8d29c2f8fb43b8e7f
Daniel Keen <keendaniel01@gmail.com>
2018-04-09 18:28:49 +0100

    :sparkles: feat: top_tweets show on output view

--- frontend/src/components/OutputView.js
+++ frontend/src/components/OutputView.js
@@ -1,58 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
 import SubHeader from './SubHeader';
 import Sentiment from './Sentiment';
 import Button from './Button';
 import { BarChart, PieChart } from 'react-easy-chart';
 import { TwitterTweetEmbed } from 'react-twitter-embed';
 
-const OutputView = props => {
-  return (
-    <div>
-      <SubHeader subHeaderText="So, the world thinks..." />
-      <Sentiment average_sentiment={props.average_sentiment} />
-      <TwitterTweetEmbed
-        id="positiveTweet"
-        tweetId={props.top_tweets.positive}
-      />
-      <TwitterTweetEmbed
-        id="negativeTweet"
-        tweetId={props.top_tweets.negative}
-      />
-      <BarChart
-        axes
-        height={350}
-        width={650}
-        data={[
-          {
-            x: 'Positive',
-            y: props.sentiments.positive,
-            color: 'green'
-          },
-          {
-            x: 'Neutral',
-            y: props.sentiments.neutral,
-            color: 'yellow'
-          },
-          {
-            x: 'Negative',
-            y: props.sentiments.negative,
-            color: 'red'
-          }
-        ]}
-      />
-      <PieChart
-        labels
-        data={[
-          { key: 'Positive', value: props.positivity_percentage },
-          { key: 'Negative', value: 100 - props.positivity_percentage }
-        ]}
-      />
-      <Button
-        buttonText="Choose another topic"
-        handleSubmit={props.handleSubmit}
-      />
-    </div>
-  );
-};
+class OutputView extends Component {
+  tweets = () => {
+    if (this.props.top_tweets === undefined) {
+      return <h4>loading tweets...</h4>;
+    } else {
+      return (
+        <div>
+          <TwitterTweetEmbed
+            id="positiveTweet"
+            tweetId={this.props.top_tweets.positive}
+          />
+          <TwitterTweetEmbed
+            id="negativeTweet"
+            tweetId={this.props.top_tweets.negative}
+          />
+        </div>
+      );
+    }
+  };
+  render() {
+    return (
+      <div>
+        <SubHeader subHeaderText="So, the world thinks..." />
+        <Sentiment average_sentiment={this.props.average_sentiment} />
+        {this.tweets()}
+        <BarChart
+          axes
+          height={350}
+          width={650}
+          data={[
+            {
+              x: 'Positive',
+              y: this.props.sentiments.positive,
+              color: 'green'
+            },
+            {
+              x: 'Neutral',
+              y: this.props.sentiments.neutral,
+              color: 'yellow'
+            },
+            {
+              x: 'Negative',
+              y: this.props.sentiments.negative,
+              color: 'red'
+            }
+          ]}
+        />
+        <PieChart
+          labels
+          data={[
+            { key: 'Positive', value: this.props.positivity_percentage },
+            { key: 'Negative', value: 100 - this.props.positivity_percentage }
+          ]}
+        />
+        <Button
+          buttonText="Choose another topic"
+          handleSubmit={this.props.handleSubmit}
+        />
+      </div>
+    );
+  }
+}
 
 export default OutputView;
