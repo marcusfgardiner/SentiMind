c5f7223dd35ecbd858470aeb047e10719899bdde
Daniel Keen <keendaniel01@gmail.com>
2018-04-09 18:16:29 +0100

    :lipstick: refactor: added alt text for logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from '../logo.png';
 
 const Header = () => {
-  return <img src={logo} />;
+  return <img alt="Sentimind logo" src={logo} />;
 };
 
 export default Header;
