c626450df39fe661a3397259eaa8ef75021ef732
Daniel Keen <keendaniel01@gmail.com>
2018-04-10 14:24:36 +0100

    :lipstick: style: victory graph styled

--- frontend/src/components/OutputView.js
+++ frontend/src/components/OutputView.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import SubHeader from './SubHeader';
 import Sentiment from './Sentiment';
 import Button from './Button';
-import { BarChart, PieChart } from 'react-easy-chart';
+import { VictoryBar, VictoryChart, VictoryAxis } from 'victory';
 import { TwitterTweetEmbed } from 'react-twitter-embed';
 
 class OutputView extends Component {
+  // <PieChart
+  //   labels
+  //   data={[
+  //     { key: 'Positive', value: this.props.positivity_percentage },
+  //     { key: 'Negative', value: 100 - this.props.positivity_percentage }
+  //   ]}
+  // />
+
+  componentWillUnmount() {
+    window.clearInterval(this.setStateInterval);
+  }
+
   firstTweet = () => {
     if (this.props.top_tweets === undefined) {
       return <h4>loading tweets...</h4>;
@@ -44,6 +56,26 @@ class OutputView extends Component {
     }
   };
   render() {
+    const data = [
+      {
+        sentiment: 'Positive',
+        percentage: this.props.sentiments.positive,
+        fill: '#70D4B4'
+      },
+      {
+        sentiment: 'Neutral',
+        percentage: this.props.sentiments.neutral,
+        fill: '#FFFCEF'
+      },
+      {
+        sentiment: 'Negative',
+        percentage: this.props.sentiments.negative,
+        fill: '#FF7F5B'
+      }
+    ];
+    if (this.props.top_tweets === undefined) {
+      return <h1>Loading...</h1>;
+    }
     return (
       <div className="container">
         {this.firstTweet()}
@@ -53,39 +85,43 @@ class OutputView extends Component {
             subHeaderText="So, the world thinks..."
           />
           <Sentiment average_sentiment={this.props.average_sentiment} />
-          <BarChart
-            height={350}
-            width={650}
-            data={[
-              {
-                x: 'Positive',
-                y: this.props.sentiments.positive,
-                color: 'green'
-              },
-              {
-                x: 'Neutral',
-                y: this.props.sentiments.neutral,
-                color: 'yellow'
-              },
-              {
-                x: 'Negative',
-                y: this.props.sentiments.negative,
-                color: 'red'
-              }
-            ]}
-          />
-          <div className="inner-grid">
-            <p>{this.props.sentiments.positive}</p>
-            <p>{this.props.sentiments.neutral}</p>
-            <p>{this.props.sentiments.negative}</p>
+          <div id="bar-chart">
+            <VictoryChart
+              domainPadding={20}
+              width={350}
+              height={250}
+              animate={{ duration: 600, easing: 'bounce' }}
+            >
+              <VictoryAxis
+                style={{
+                  axis: { stroke: '#E0F2F1' },
+                  axisLabel: { fontSize: 16, fill: '#E0F2F1' },
+                  ticks: { stroke: '#ccc' },
+                  tickLabels: {
+                    fontSize: 14,
+                    fill: '#E0F2F1',
+                    fontWeight: 'bold'
+                  },
+                  grid: { stroke: '#B3E5FC', strokeWidth: 0.25 }
+                }}
+                dependentAxis
+              />
+              <VictoryAxis
+                style={{
+                  axis: { stroke: '#E0F2F1' },
+                  axisLabel: { fontSize: 16 },
+                  ticks: { stroke: '#ccc' },
+                  tickLabels: {
+                    fontSize: 10,
+                    fill: '#E0F2F1',
+                    fontWeight: 'bold'
+                  }
+                }}
+              />
+              <VictoryBar data={data} x="sentiment" y="percentage" />
+            </VictoryChart>
           </div>
-          <PieChart
-            labels
-            data={[
-              { key: 'Positive', value: this.props.positivity_percentage },
-              { key: 'Negative', value: 100 - this.props.positivity_percentage }
-            ]}
-          />
+
           <Button
             buttonText="Choose another topic"
             handleSubmit={this.props.handleSubmit}
