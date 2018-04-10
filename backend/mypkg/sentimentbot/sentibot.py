dfa8d0ecf8387f308eb03c609dd9a1cdacf31d31
marcusfgardiner <marcusfgardiner@gmail.com>
2018-04-10 17:15:01 +0100

    :sparkles: feat: adding support for different ML models

--- backend/mypkg/sentimentbot/sentibot.py
+++ backend/mypkg/sentimentbot/sentibot.py
@@ -4,16 +4,18 @@ import nltk
 import numpy
 import sklearn as skl
 import pickle
-
+from sklearn.naive_bayes import MultinomialNB,BernoulliNB
+from nltk.classify.scikitlearn import SklearnClassifier
+from sklearn.linear_model import LogisticRegression,SGDClassifier
+from sklearn.svm import SVC, LinearSVC, NuSVC
 
 df = pd.DataFrame(pd.read_csv('testdata.csv'))
 
-
-sentiment_column = (df.iloc[:, [0]])
+sentiment_column = (df.iloc[:, [1]])
 sentiment_array = sentiment_column.values
 
 
-text_column = (df.iloc[:, [5]])
+text_column = (df.iloc[:, [6]])
 text_array = text_column.values
 
 text = []
@@ -62,18 +64,62 @@ def extract_features(text):
 
 # print (extract_features(['love','reading','incredibly']))
 
-training_set = nltk.classify.apply_features(extract_features, tweets)
+full_data_set = nltk.classify.apply_features(extract_features, tweets)
+
+# set that we'll train our classifier with
+training_set = full_data_set[:400]
+
+# set that we'll test against.
+testing_set = full_data_set[400:]
 
 # print (training_set)
 
 classifier = nltk.NaiveBayesClassifier.train(training_set)
+print("MultinomialNB accuracy percent:",nltk.classify.accuracy(classifier, testing_set))
 
 # print (classifier.show_most_informative_features(32))
 
-test_tweet = 'jquery museum'
+test_tweet = 'fantastic awful butts'
+processed_test_tweet = extract_features(test_tweet.split())
 
-print (classifier.classify(extract_features(test_tweet.split())))
+print (classifier.classify(processed_test_tweet))
 
 f = open('my_classifier.pickle', 'wb')
 pickle.dump(classifier, f)
 f.close()
+
+
+# ------------------ ------------------ ------------------ ------------------ ------------------ ------------------
+
+# Other models
+
+print("Original Naive Bayes Algo accuracy percent:", (nltk.classify.accuracy(classifier, testing_set))*100)
+classifier.show_most_informative_features(15)
+
+MNB_classifier = SklearnClassifier(MultinomialNB())
+MNB_classifier.train(training_set)
+print("MultinomialNB accuracy percent:",nltk.classify.accuracy(MNB_classifier, testing_set))
+
+BNB_classifier = SklearnClassifier(BernoulliNB())
+BNB_classifier.train(training_set)
+print("BernoulliNB accuracy percent:",nltk.classify.accuracy(BNB_classifier, testing_set))
+
+LogisticRegression_classifier = SklearnClassifier(LogisticRegression())
+LogisticRegression_classifier.train(training_set)
+print("LogisticRegression_classifier accuracy percent:", (nltk.classify.accuracy(LogisticRegression_classifier, testing_set))*100)
+
+SGDClassifier_classifier = SklearnClassifier(SGDClassifier())
+SGDClassifier_classifier.train(training_set)
+print("SGDClassifier_classifier accuracy percent:", (nltk.classify.accuracy(SGDClassifier_classifier, testing_set))*100)
+
+SVC_classifier = SklearnClassifier(SVC())
+SVC_classifier.train(training_set)
+print("SVC_classifier accuracy percent:", (nltk.classify.accuracy(SVC_classifier, testing_set))*100)
+
+LinearSVC_classifier = SklearnClassifier(LinearSVC())
+LinearSVC_classifier.train(training_set)
+print("LinearSVC_classifier accuracy percent:", (nltk.classify.accuracy(LinearSVC_classifier, testing_set))*100)
+
+NuSVC_classifier = SklearnClassifier(NuSVC())
+NuSVC_classifier.train(training_set)
+print("NuSVC_classifier accuracy percent:", (nltk.classify.accuracy(NuSVC_classifier, testing_set))*100)
