f21b37dd1364434c097279b5c7559a27ef8f8af9
Daniel Keen <keendaniel01@gmail.com>
2018-04-12 11:18:26 +0100

    :lipstick: style: tech logos

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,9 +1,62 @@
 import React from 'react';
+import reactLogo from '../images/react-original.svg';
+import pythonLogo from '../images/python-original.svg';
+import dockerLogo from '../images/docker-original.svg';
+import amazonLogo from '../images/amazonwebservices-original.svg';
+import githubLogo from '../images/github-original.svg';
 
 const Footer = () => {
   return (
     <footer id="footer">
-      <h3>Footer</h3>
+      <p>
+        SentiMind analyses and visualises the sentiment (frequency and polarity)
+        of the latest 100 tweets using SentiBot, an algorithm trained using
+        machine learning
+      </p>
+
+      <p>
+        Powered by{' '}
+        <img
+          className="logos"
+          alt="react logo"
+          width="40"
+          height="40"
+          src={reactLogo}
+        />{' '}
+        and{' '}
+        <img
+          className="logos"
+          alt="python logo"
+          width="40"
+          height="40"
+          src={pythonLogo}
+        />{' '}
+        deployed with{' '}
+        <img
+          className="logos"
+          alt="python logo"
+          width="40"
+          height="40"
+          src={dockerLogo}
+        />{' '}
+        and{' '}
+        <img
+          className="logos"
+          alt="python logo"
+          width="40"
+          height="40"
+          src={amazonLogo}
+        />{' '}
+        check our{' '}
+        <img
+          className="logos"
+          alt="python logo"
+          width="40"
+          height="40"
+          src={githubLogo}
+        />{' '}
+        for more info.
+      </p>
     </footer>
   );
 };
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../logo.png';
+import logo from '../images/logo.png';
 
 const Header = () => {
   return <img alt="Sentimind logo" src={logo} />;
