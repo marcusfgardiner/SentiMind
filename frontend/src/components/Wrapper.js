78594b13618a1eb29e99af1a14ff92842510b381
Daniel Keen <keendaniel01@gmail.com>
2018-04-12 10:37:50 +0100

    :white_check_mark: test: footer matches snapshot

--- frontend/src/components/Wrapper.js
+++ frontend/src/components/Wrapper.js
@@ -40,7 +40,7 @@ class Wrapper extends Component {
   fetchSentiment = async () => {
     let { query } = this.state;
     try {
-      const request = await fetch('http://sentimind.co.uk:5000/', {
+      const request = await fetch(`${this.createUrl()}:5000/`, {
         method: 'POST',
         body: JSON.stringify(query),
         headers: new Headers({
